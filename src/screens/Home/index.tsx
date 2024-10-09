5e3f13afee156dc935ec19a32efddd2a7cb5da43
Matheus Tiburcio <matheusgtiburcio@gmail.com>
2024-10-09 19:31:19 -0300

    first commit

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function Home() {
+  return (
+    <View className="items-center justify-center flex h-full">
+      <Text>Home</Text>
+    </View>
+  );
+}
